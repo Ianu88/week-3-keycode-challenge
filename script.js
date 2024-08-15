e69ee6ffd2de2604cbd43bf96737793e564c229d
IanUpton <uptonian22@gmail.com>
2024-08-15 15:11:24 +0100

    notes added for detail and extraneous location variable removed

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ let head = document.getElementById('keycode');
 
 function keycode(event) {
 let key = event.key;
-let location = event.location;
 let which = event.which;
 let code = event.code;
 head.textContent = which;
 document.getElementById("eventKey").innerHTML= key;
-document.getElementById("eventLocation").innerHTML= location;
 document.getElementById("eventWhich").innerHTML= which;
 document.getElementById("eventCode").innerHTML= code;
 welcome.remove()
